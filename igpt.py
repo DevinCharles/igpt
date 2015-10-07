511afdb21d202ad434cd2fc9c3ef416671551ef8
Devin Prescott <devincprescott@gmail.com>
2015-10-07 03:05:27 -0400

    First commit

---
+++ igpt.py
@@ -0,0 +1,35 @@
+"""
+Blah Blah Blah....
+"""
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from future import standard_library
+standard_library.install_aliases()
+import sys
+import getopt
+
+from grids import shapefileGrid
+from grids import blockIndexer
+from examples import mapGenerator
+from examples.examples import grids_example
+
+def main():
+    # parse command line options
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+    except Exception as e:
+        print(e)
+        print("for help use --help")
+        sys.exit(2)
+    # process options
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print(__doc__)
+            sys.exit(0)
+    # process arguments
+    for arg in args:
+        process(arg) # process() is defined elsewhere
+
+if __name__ == "__main__":
+    main()
